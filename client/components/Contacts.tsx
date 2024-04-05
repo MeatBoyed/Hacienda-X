013f95af9de37ae58221ceea530ae7f3188fd738
Daniel Akilo <kayodeakilo1@gmail.com>
2024-04-05 19:45:06 +0200

    Added Contacts.tsx file and code

---
+++ client/components/Contacts.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Contact.css";
+import data from "../../utils/contactslider.json";
+
+const Contact = () => {
+  return (
+    <section className="r-wrapper">
+      <div className="paddings innerWidth r-container">
+        <div className="r-head contact-container">
+          <span className="orangeText">Contact Our Agents</span>
+          <span className="primaryText">Meet Our Dedicated Team</span>
+        </div>
+
+        <div className="r-card contact-card">
+          {data.map((card, i) => (
+            <div key={i} className="contact-card-container flexStart">
+              <div className="circle-image">
+                <img src={card.image} alt="agent" />
+              </div>
+              <div className="contact-details">
+                <span className="primaryText">{card.name}</span>
+                <span className="secondaryText">{card.phone}</span>
+                <button className="button">Contact Now</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
