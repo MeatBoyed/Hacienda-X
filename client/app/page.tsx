60cbfba4dea45ce105626103af5754f3dfcd8936
Daniel Akilo <kayodeakilo1@gmail.com>
2024-04-06 16:17:03 +0200

    Update: updated the page.tsx  to display contacts

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import Residencies from "@/components/Residencies";
 import Rentals from "@/components/Rentals";
 import AboutUs from "@/components/AboutUs";
+import Contact from "@/components/Contacts";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Residencies />
         <Rentals />
         <AboutUs />
+        <Contact />
       </div>
     </main>
   );
