7e7548d967bc695ec3f61abe217ef3bffa7c7263
Daniel Akilo <kayodeakilo1@gmail.com>
2024-04-06 16:34:04 +0200

    Feature: Added a call to action section. Created CallToAction.tsx file

---
+++ client/components/CallToAction.tsx
@@ -0,0 +1,13 @@
+// CallToAction.jsx
+import React from "react";
+
+const CallToAction = () => {
+  return (
+    <section className="call-to-action">
+      <h1 className="primaryText">Want To Add Your Properties?</h1>
+      <button className="learn-more-button">Learn More</button>
+    </section>
+  );
+};
+
+export default CallToAction;
