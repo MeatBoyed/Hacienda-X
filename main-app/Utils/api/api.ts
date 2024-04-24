b5c24d538756c1b5ae94f18784f92f77c4a6b11e
MeatBoyed <charlierossouw@outlook.com>
2024-04-24 15:30:08 +0200

    fix: Fixes for Production

--- main-app/Utils/api/api.ts
+++ main-app/Utils/api/api.ts
@@ -23,20 +23,20 @@ export async function getAllProperties() {
 // }
 export const createProperty = async () => {
   try {
-    // const response = await db.property.findMany();
-    const response = await db.property.create({
-      data: {
-        title: "Modern Apartment in Downtown",
-        description:
-          "A stylish and modern 2-bedroom apartment located in the heart of the city.",
-        price: 200000,
-        address: "123 Main St",
-        city: "New York",
-        country: "USA",
-        image: "https://example.com/apartment1.jpg",
-        facilities: ["parking", "gym", "pool"],
-      },
-    });
+    const response = await db.property.findMany();
+    // const response = await db.property.create({
+    //   data: {
+    //     title: "Modern Apartment in Downtown",
+    //     description:
+    //       "A stylish and modern 2-bedroom apartment located in the heart of the city.",
+    //     price: 200000,
+    //     address: "123 Main St",
+    //     city: "New York",
+    //     country: "USA",
+    //     image: "https://example.com/apartment1.jpg",
+    //     facilities: ["parking", "gym", "pool"],
+    //   },
+    // });
 
     // IMPLEMENT
     //   if (response.status === 400 || response.status === 500) {
