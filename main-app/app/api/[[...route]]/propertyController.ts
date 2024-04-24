373d663d760d86249cf41e5b5f5dde71cfab7338
MeatBoyed <charlierossouw@outlook.com>
2024-04-24 13:46:33 +0200

    feat: GET property by Slug is a functional route

--- main-app/app/api/[[...route]]/propertyController.ts
+++ main-app/app/api/[[...route]]/propertyController.ts
@@ -3,11 +3,13 @@ import db from "./db";
 
 const app = new Hono();
 
+// Endpoint ("/api/properties")
 app.get("/", async (c) => {
   try {
+    // Database query (obvs)
     const properties = await db.property.findMany();
-    console.log("HELLOO!: ", properties);
 
+    // Response object
     return c.json(
       { results: properties },
       {
@@ -15,7 +17,51 @@ app.get("/", async (c) => {
       }
     );
   } catch (error: any) {
+    // Use as a "Catch-All" error handler
+
+    // Show error in console for Debugging (Realistically this should be logged used a package)
     console.log(error);
+
+    // Respond with an Error for Client "error" state
+    throw new Error("Something went wrong. Error: ", error);
+  }
+});
+
+// Endpoint ("/api/properties/:slug")
+// - The slug will be the public, Search Engine Optimized, unique identifier for Properties.
+app.get("/:slug", async (c) => {
+  try {
+    const slug = c.req.param("slug");
+
+    // Database query (obvs)
+    const prop = await db.property.findFirst({
+      where: { title: slug }, // Property needs a Slug field in DB
+    });
+
+    // Let the Client (Front-End) decide what to do with a 404
+    if (!prop) {
+      return c.json(
+        { results: undefined },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    // Response object
+    return c.json(
+      { results: prop },
+      {
+        status: 200,
+      }
+    );
+  } catch (error: any) {
+    // Use as a "Catch-All" error handler
+
+    // Show error in console for Debugging (Realistically this should be logged used a package)
+    console.log(error);
+
+    // Respond with an Error for Client "error" state
     throw new Error("Something went wrong. Error: ", error);
   }
 });
