ecdf0c2c40ea1e89c7a1132bf883d73396af88f3
MeatBoyed <charlierossouw@outlook.com>
2024-04-26 15:40:18 +0200

    feat: ShadCN carousel functional in PropertyCarousel

--- main-app/components/PropertiesCarousel.tsx
+++ main-app/components/PropertiesCarousel.tsx
@@ -7,6 +7,14 @@ import { useSwiper } from "swiper/react";
 import PropertyCard from "./PropertyCard";
 import { Button } from "./ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Card, CardContent } from "./ui/card";
 
 export default function PropertiesCarousel({
   data,
@@ -41,3 +49,26 @@ const SlideNextButton = () => {
     </div>
   );
 };
+
+export function PropertyCarousel2({ data }: { data: Property[] | null }) {
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full "
+    >
+      <CarouselContent className="w-full gap-5 -ml-4">
+        {/* {Array.from({ length: 5 }).map((_, index) => ( */}
+        {/* {data?.slice(0, 8).map((property, index) => ( */}
+        {data?.map((property, index) => (
+          <div className="pl-4 w-full h-full">
+            <PropertyCard property={property} key={index} />
+          </div>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
