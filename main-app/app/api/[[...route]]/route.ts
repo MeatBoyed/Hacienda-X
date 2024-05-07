764204208e14c799e574aa2ded31fccb2bdd1079
MeatBoyed <charlierossouw@outlook.com>
2024-05-07 10:14:42 +0200

    feat: Onboarding functional, backend API needs config

--- main-app/app/api/[[...route]]/route.ts
+++ main-app/app/api/[[...route]]/route.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import propertyController from "./propertyController";
+import userController from "./userController";
 import webhooks from "./webhooks";
 
 // Increase performance by using either Drizzle, or Prisma Accelerate
@@ -9,6 +10,7 @@ import webhooks from "./webhooks";
 const app = new Hono().basePath("/api");
 
 app.route("/properties", propertyController);
+app.route("/user", userController);
 // app.route("/webhooks", webhooks);
 
 export const GET = handle(app);
