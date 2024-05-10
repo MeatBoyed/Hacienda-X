b10747325e518f90f3d71ea1b28a4c69b11efd9a
Daniel Akilo <kayodeakilo1@gmail.com>
2024-05-10 16:36:04 +0200

    Created About Us Page

--- main-app/app/aboutus/page.tsx
+++ main-app/app/aboutus/page.tsx
@@ -21,58 +21,45 @@ import data from "@/Utils/accordion";
 export default function Value() {
   const [className, setClassName] = useState<string | null>(null);
   return (
-    <section id="value" className="v-wrapper">
-      <div className="paddings innerWidth flexCenter v-container">
-        {/* right */}
-        <div className="flexColStart v-right">
-          <span className="orangeText">Our Value</span>
-
-          <span className="primaryText">Value We Give to You</span>
-
-          <span className="secondaryText">
-            We always ready to help by providijng the best services for you.
-            <br />
-            We beleive a good blace to live can make your life better
-          </span>
-
-          <Accordion
-            className="accordion"
-            allowMultipleExpanded={false}
-            preExpanded={[0]}
-          >
-            {data.map((item, i) => {
-              return (
-                <AccordionItem
-                  className={`accordionItem ${className}`}
-                  uuid={i}
-                  key={i}
-                >
-                  <AccordionItemHeading>
-                    <AccordionItemButton className="flexCenter accordionButton ">
-                      {/* just for getting state of item */}
-                      {/* <AccordionItemState>
-                    {({ expanded }) =>
-                      expanded
-                        ? setClassName("expanded")
-                        : setClassName("collapsed")
-                    }
-                  </AccordionItemState> */}
-                      <div className="flexCenter icon">{item.icon}</div>
-                      <span className="primaryText">{item.heading}</span>
-                      <div className="flexCenter icon">
-                        <MdOutlineArrowDropDown size={20} />
-                      </div>
-                    </AccordionItemButton>
-                  </AccordionItemHeading>
-                  <AccordionItemPanel>
-                    <p className="secondaryText">{item.detail}</p>
-                  </AccordionItemPanel>
-                </AccordionItem>
-              );
-            })}
-          </Accordion>
+    <div className="bg-white dark:bg-gray-800 overflow-hidden relative lg:flex lg:items-center">
+      <div className="w-full py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+        <h2 className="text-3xl font-bold text-black dark:text-white sm:text-4xl">
+          <span className="block">HaciendaX Real Estate</span>
+        </h2>
+        <p className="text-md mt-4 text-gray-400">
+          We're committed to empowering buyers and sellers with knowledge,
+          support, and personalized service. At Hacienda, our passion for people
+          and properties drives us to exceed expectations and create lasting
+          relationships. <br />
+          Discover what sets us apart at Hacienda. We strive for excellence in
+          every transaction, aiming to surpass industry standards and client
+          expectations. With a focus on integrity, transparency, and
+          professionalism, we're dedicated to achieving your real estate goals
+          and earning your trust.
+        </p>
+        <div className="lg:mt-0 lg:flex-shrink-0">
+          <div className="mt-12 inline-flex rounded-md shadow">
+            <button
+              type="button"
+              className="py-2 px-4  bg-blue-500 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+            >
+              Get started
+            </button>
+          </div>
         </div>
       </div>
-    </section>
+      <div className="flex items-center gap-8 p-8 lg:p-24">
+        <img
+          src="./r1.png"
+          className="w-1/2 rounded-lg"
+          alt="TrHouse Imageee"
+        />
+        <div>
+          <img src="./r3.png" className="mb-8 rounded-lg" alt="House Image" />
+          <img src="./r2.png" className="rounded-lg" alt="House Image" />
+        </div>
+      </div>
+      <div></div>
+    </div>
   );
 }
