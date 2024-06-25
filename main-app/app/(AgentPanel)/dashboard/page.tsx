332855345f37b4e6fdad15fa05099c319431325a
MeatBoyed <charlierossouw@outlook.com>
2024-06-25 18:05:18 +0200

    feat: Redirect to dashboard/property on /dashbaord nav

--- main-app/app/(AgentPanel)/dashboard/page.tsx
+++ main-app/app/(AgentPanel)/dashboard/page.tsx
@@ -5,40 +5,43 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Eye, TrendingUp } from "lucide-react";
-import NewLeads from "./_components/NewLeads";
-import PropertyInsightCard from "./_components/PropertyInsightCard";
-import InsightCard from "./_components/InsightCard";
+import { redirect } from "next/navigation";
 
 // Tremor for Analytics Components - https://www.tremor.so/
 
 export default function Dashboard() {
   // Auth user via Clerk & in Dashboard
   // Track via Posthog & set role
+
+  // const router = useRouter();
+  // router.push("/dashboard/property");
+
+  redirect("/dashboard/property");
+
   return (
     <div className="w-full flex justify-start items-start gap-5 pb-20  my-10 flex-col h-[100vh]">
       <p className="scroll-m-20 text-2xl font-semibold tracking-tight">
         Overview
       </p>
 
-      <section className="w-full flex justify-between items-start ">
-        {/* Left Side */}
-        <div className="flex justify-center items-start flex-col">
-          <div className=" flex justify-center items-center gap-4">
-            {/* <InsightCard /> */}
-            {/* <InsightCard />
+      {/* <section className="w-full flex justify-between items-start "> */}
+      {/* Left Side */}
+      {/* <div className="flex justify-center items-start flex-col">
+          <div className=" flex justify-center items-center gap-4"> */}
+      {/* <InsightCard /> */}
+      {/* <InsightCard />
             <InsightCard /> */}
-          </div>
+      {/* </div>
 
           <div className="w-full h-[40vh] bg-gray-600 mt-10 rounded-lg" />
-        </div>
+        </div> */}
 
-        {/* Right */}
-        <div className="">
+      {/* Right */}
+      {/* <div className="">
           <LeadInsightCard />
           <NewLeads />
-        </div>
-      </section>
+        </div> */}
+      {/* </section> */}
 
       <p className="scroll-m-20 text-2xl font-semibold tracking-tight">
         Your properties
