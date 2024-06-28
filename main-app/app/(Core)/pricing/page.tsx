1e863df68dd336c1af82cda8265447cdc2f8b70c
Daniel Akilo <kayodeakilo1@gmail.com>
2024-06-28 18:02:54 +0200

    Prcing page, faq and table

--- main-app/app/(Core)/pricing/page.tsx
+++ main-app/app/(Core)/pricing/page.tsx
@@ -1,6 +1,6 @@
 // pricingplan.tsx
 "use client";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import "./pricing.css";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
@@ -13,7 +13,7 @@ interface PricingCardProps {
   subscription: string;
   buttonText: string;
   active?: boolean;
-  className: string; // Added className property
+  classNameName: string; // Added classNameName property
 }
 
 const Pricing = () => {
@@ -44,7 +44,7 @@ const Pricing = () => {
               subscription="year"
               description="Best for large real estate firms needing comprehensive features."
               buttonText="Choose Enterprise"
-              className="border-solid border-4 border-blue-600"
+              classNameName="border-solid border-4 border-blue-600"
             >
               <List>Unlimited Property Listings</List>
               <List>Maximum Exposure</List>
@@ -61,7 +61,7 @@ const Pricing = () => {
               description="Perfect for small real estate teams."
               buttonText="Choose Pro"
               active
-              className="border-solid border-4 border-black"
+              classNameName="border-solid border-4 border-black"
             >
               <List>10 Property Listings</List>
               <List>Enhanced Exposure</List>
@@ -75,7 +75,7 @@ const Pricing = () => {
               subscription="month"
               description="Ideal for individual agents starting out."
               buttonText="Choose Basic"
-              className="border-solid border-4 border-gray-400"
+              classNameName="border-solid border-4 border-gray-400"
             >
               <List>1 Property Listing</List>
               <List>Standard Exposure</List>
@@ -99,13 +99,13 @@ const PricingCard: React.FC<PricingCardProps> = ({
   subscription,
   buttonText,
   active,
-  className, // Added className here
+  classNameName, // Added classNameName here
 }) => {
   return (
     <>
       <div className="w-full px-4 md:w-1/2 lg:w-1/3">
         <div
-          className={`relative z-10 mb-10 overflow-hidden rounded-[10px] border-2 border-stroke bg-white px-8 py-10 shadow-pricing dark:border-dark-3 dark:bg-dark-2 sm:p-12 lg:px-6 lg:py-10 xl:p-[50px] ${className}`}
+          className={`relative z-10 mb-10 overflow-hidden rounded-[10px] border-2 border-stroke bg-white px-8 py-10 shadow-pricing dark:border-dark-3 dark:bg-dark-2 sm:p-12 lg:px-6 lg:py-10 xl:p-[50px] ${classNameName}`}
         >
           <span className="mb-3 block text-lg font-semibold text-primary">
             {type}
@@ -400,5 +400,417 @@ const PricingCard: React.FC<PricingCardProps> = ({
 const List: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <p className="text-base text-body-color dark:text-dark-6">{children}</p>
+  );
+};
+
+<section className="dark:bg-gray-100 dark:text-gray-800">
+  <div className="container mx-auto p-6 overflow-x-auto">
+    <table className="w-full">
+      <caption className="sr-only">Pricing plan comparison</caption>
+      <thead>
+        <tr>
+          <th></th>
+          <th scope="col">
+            <h2 className="px-2 text-lg font-medium">Starter</h2>
+            <p className="mb-3">
+              <span className="text-2xl font-bold sm:text-4xl dark:text-gray-900">
+                0€
+              </span>
+              <span className="font-medium dark:text-gray-600">/mo</span>
+            </p>
+          </th>
+          <th scope="col">
+            <h2 className="px-2 text-lg font-medium">Standard</h2>
+            <p className="mb-3">
+              <span className="text-2xl font-bold sm:text-4xl dark:text-gray-900">
+                19€
+              </span>
+              <span className="font-medium dark:text-gray-600">/mo</span>
+            </p>
+          </th>
+          <th scope="col">
+            <h2 className="px-2 text-lg font-medium">Premium</h2>
+            <p className="mb-3">
+              <span className="text-2xl font-bold sm:text-4xl dark:text-gray-900">
+                49€
+              </span>
+              <span className="font-medium dark:text-gray-600">/mo</span>
+            </p>
+          </th>
+        </tr>
+      </thead>
+      <tbody className="space-y-6 text-center divide-y dark:divide-gray-300">
+        <tr>
+          <th scope="row" className="text-left">
+            <h3 className="py-3">Euismod</h3>
+          </th>
+          <td>
+            <span className="block text-sm">1</span>
+          </td>
+          <td>
+            <span className="block text-sm">10</span>
+          </td>
+          <td>
+            <span className="block text-sm">100</span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-left">
+            <h3 className="py-3">Principes et</h3>
+          </th>
+          <td>
+            <span className="block text-sm">0,5 GB</span>
+          </td>
+          <td>
+            <span className="block text-sm">5 GB</span>
+          </td>
+          <td>
+            <span className="block text-sm">500 GB</span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-left">
+            <h3 className="py-3">Et mel porro</h3>
+          </th>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Included in Free plan"
+              className="w-5 h-5 mx-auto dark:text-violet-600"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Included in Standard plan"
+              className="w-5 h-5 mx-auto dark:text-violet-600"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Included in Premium plan"
+              className="w-5 h-5 mx-auto dark:text-violet-600"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-left">
+            <h3 className="py-3">Veniam suscipiantur</h3>
+          </th>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Not included in Free plan"
+              className="w-5 h-5 mx-auto dark:text-gray-400"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Included in Standard plan"
+              className="w-5 h-5 mx-auto dark:text-violet-600"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Included in Premium plan"
+              className="w-5 h-5 mx-auto dark:text-violet-600"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-left">
+            <h3 className="py-3">Ornatus tacimates</h3>
+          </th>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Not included in Free plan"
+              className="w-5 h-5 mx-auto dark:text-gray-400"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Not included in Standard plan"
+              className="w-5 h-5 mx-auto dark:text-gray-400"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Included in Premium plan"
+              className="w-5 h-5 mx-auto dark:text-violet-600"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-left">
+            <h3 className="py-3">Aliquam fastidii in mei</h3>
+          </th>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Not included in Free plan"
+              className="w-5 h-5 mx-auto dark:text-gray-400"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Not included in Standard plan"
+              className="w-5 h-5 mx-auto dark:text-gray-400"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-label="Included in Premium plan"
+              className="w-5 h-5 mx-auto dark:text-violet-600"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</section>;
+
+const Accordion = () => {
+  return (
+    <section className="relative z-20 overflow-hidden bg-white pb-12 pt-20 dark:bg-dark lg:pb-[90px] lg:pt-[120px]">
+      <div className="container mx-auto">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4">
+            <div className="mx-auto mb-[60px] max-w-[520px] text-center lg:mb-20">
+              <span className="mb-2 block text-lg font-semibold text-primary">
+                FAQ
+              </span>
+              <h2 className="mb-4 text-3xl font-bold text-dark dark:text-white sm:text-[40px]/[48px]">
+                Any Questions? Look Here
+              </h2>
+              <p className="text-base text-body-color dark:text-dark-6">
+                There are many variations of passages of Lorem Ipsum available
+                but the majority have suffered alteration in some form.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4 lg:w-1/2">
+            <AccordionItem
+              header="How long we deliver your first blog post?"
+              text="It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available ."
+            />
+            <AccordionItem
+              header="How long we deliver your first blog post?"
+              text="It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available ."
+            />
+            <AccordionItem
+              header="How long we deliver your first blog post?"
+              text="It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available ."
+            />
+          </div>
+          <div className="w-full px-4 lg:w-1/2">
+            <AccordionItem
+              header="How long we deliver your first blog post?"
+              text="It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available ."
+            />
+            <AccordionItem
+              header="How long we deliver your first blog post?"
+              text="It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available ."
+            />
+            <AccordionItem
+              header="How long we deliver your first blog post?"
+              text="It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available ."
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute bottom-0 right-0 z-[-1]">
+        <svg
+          width="1440"
+          height="886"
+          viewBox="0 0 1440 886"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            opacity="0.5"
+            d="M193.307 -273.321L1480.87 1014.24L1121.85 1373.26C1121.85 1373.26 731.745 983.231 478.513 729.927C225.976 477.317 -165.714 85.6993 -165.714 85.6993L193.307 -273.321Z"
+            fill="url(#paint0_linear)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear"
+              x1="1308.65"
+              y1="1142.58"
+              x2="602.827"
+              y2="-418.681"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#3056D3" stop-opacity="0.36" />
+              <stop offset="1" stop-color="#F5F2FD" stop-opacity="0" />
+              <stop offset="1" stop-color="#F5F2FD" stop-opacity="0.096144" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+    </section>
+  );
+};
+
+interface AccordionItemProps {
+  header: string;
+  text: string;
+}
+
+const AccordionItem: React.FC<AccordionItemProps> = ({ header, text }) => {
+  const [active, setActive] = useState(false);
+
+  const handleToggle = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setActive(!active);
+  };
+
+  return (
+    <div className="mb-8 w-full rounded-lg bg-white p-4 shadow-[0px_20px_95px_0px_rgba(201,203,204,0.30)] dark:bg-dark-2 dark:shadow-[0px_20px_95px_0px_rgba(0,0,0,0.30)] sm:p-8 lg:px-6 xl:px-8">
+      <button
+        className={`faq-btn flex w-full text-left`}
+        onClick={handleToggle}
+      >
+        <div className="mr-5 flex h-10 w-full max-w-[40px] items-center justify-center rounded-lg bg-primary/5 text-primary dark:bg-white/5">
+          <svg
+            className={`fill-primary stroke-primary duration-200 ease-in-out ${
+              active ? "rotate-180" : ""
+            }`}
+            width="17"
+            height="10"
+            viewBox="0 0 17 10"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.28687 8.43257L7.28679 8.43265L7.29496 8.43985C7.62576 8.73124 8.02464 8.86001 8.41472 8.86001C8.83092 8.86001 9.22376 8.69083 9.53447 8.41713L9.53454 8.41721L9.54184 8.41052L15.7631 2.70784L15.7691 2.70231L15.7749 2.69659C16.0981 2.38028 16.1985 1.80579 15.7981 1.41393C15.4803 1.1028 14.9167 1.00854 14.5249 1.38489L8.41472 7.00806L2.29995 1.38063L2.29151 1.37286L2.28271 1.36548C1.93092 1.07036 1.38469 1.06804 1.03129 1.41393L1.01755 1.42738L1.00488 1.44184C0.69687 1.79355 0.695778 2.34549 1.0545 2.69659L1.05999 2.70196L1.06565 2.70717L7.28687 8.43257Z"
+              fill=""
+              stroke=""
+            />
+          </svg>
+        </div>
+        <div className="w-full">
+          <h4 className="mt-1 text-lg font-semibold text-dark dark:text-white">
+            {header}
+          </h4>
+        </div>
+      </button>
+
+      <div
+        className={`pl-[62px] duration-200 ease-in-out ${
+          active ? "block" : "hidden"
+        }`}
+      >
+        <p className="py-3 text-base leading-relaxed text-body-color dark:text-dark-6">
+          {text}
+        </p>
+      </div>
+    </div>
   );
 };
