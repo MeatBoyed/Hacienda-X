895f81eab895cc5a9f5f135618d49e410d0aa89b
Daniel Akilo <kayodeakilo1@gmail.com>
2024-07-02 17:08:21 +0200

    Fixed the contact page for the 3rd time

--- main-app/app/(Core)/contactus/page.tsx
+++ main-app/app/(Core)/contactus/page.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const Contactus = () => {
   return (
     <div className="bg-gray-900 min-h-screen">
-      <div className="bg-gray-100 py-12">
+      <div className="bg-gray-900 pt-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="lg:text-center">
             <h2 className="text-base text-blue-600 font-semibold tracking-wide uppercase"></h2>
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
               Hacienda
             </p>
             <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
