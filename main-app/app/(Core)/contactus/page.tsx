dde6aef78266b2731718f5018a9870aa988b4406
Daniel Akilo <kayodeakilo1@gmail.com>
2024-07-02 17:17:52 +0200

    Hopefully last commit for the designing of the contacts page

--- main-app/app/(Core)/contactus/page.tsx
+++ main-app/app/(Core)/contactus/page.tsx
@@ -84,6 +84,10 @@ const Contactus = () => {
           </div>
 
           <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-xl">
+            <h2 className="text-2xl font-bold text-black mb-2">
+              Write your enquiry below
+            </h2>
+            <p className="text-gray-500 mb-4">We are here to assist you</p>
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
