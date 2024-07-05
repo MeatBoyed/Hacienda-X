d91941c9db412246b41371309dcce9a2af98cbea
Daniel Akilo <kayodeakilo1@gmail.com>
2024-07-05 16:26:23 +0200

    fixing positioning of signup button in home page howitworks

--- main-app/app/_components/HowItWorks.tsx
+++ main-app/app/_components/HowItWorks.tsx
@@ -17,8 +17,8 @@ export default function HowItWorks() {
             <h2 className="text-2xl font-bold mb-4">Sell Property</h2>
             <p className="mb-4">
               Sell Your Property Now! Personalized property creation. Control
-              all your properties from the tips of your fingertips. Do not miss
-              out on your home being sold – Sell today!
+              all your properties from the tips of your fingertips. <br />
+              Do not miss out on your home being sold – Sell today!
             </p>
             <a
               href="./sign-in"
