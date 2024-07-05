971b19585e9d814c2bcf6923e6bc24171282005e
Daniel Akilo <kayodeakilo1@gmail.com>
2024-07-05 16:38:02 +0200

    fixing tablet responsiveness for howitwrks section

--- main-app/app/_components/HowItWorks.tsx
+++ main-app/app/_components/HowItWorks.tsx
@@ -3,7 +3,7 @@ import cta1 from "@/ctapic1/r1.jpg";
 
 export default function HowItWorks() {
   return (
-    <section className="pt-14 gap-5 max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <section className="pt-14 gap-5 max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
       <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105">
         <div className="article-wrapper p-4">
           <figure className="mb-4">
@@ -14,20 +14,20 @@ export default function HowItWorks() {
             />
           </figure>
           <div className="article-body text-center">
-            <h2 className="text-2xl font-bold mb-4">Sell Property</h2>
-            <p className="mb-4">
+            <h2 className="text-xl md:text-lg font-bold mb-4">Sell Property</h2>
+            <p className="mb-4 text-sm md:text-xs">
               Sell Your Property Now! Personalized property creation. Control
-              all your properties from the tips of your fingertips. <br />
-              Do not miss out on your home being sold – Sell today!
+              all your properties from the tips of your fingertips. Do not miss
+              out on your home being sold – Sell today!
             </p>
             <a
               href="./sign-in"
-              className="button1 read-more inline-flex items-center justify-center px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+              className="button1 read-more inline-flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-sm md:text-xs"
             >
               Sign UP<span className="sr-only">about this is some title</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5 ml-2"
+                className="w-4 h-4 ml-2"
                 viewBox="0 0 20 20"
                 fill="currentColor"
               >
@@ -51,21 +51,23 @@ export default function HowItWorks() {
             />
           </figure>
           <div className="article-body text-center">
-            <h2 className="text-2xl font-bold mb-4">Buy Property Now!</h2>
-            <p className="mb-4">
+            <h2 className="text-xl md:text-lg font-bold mb-4">
+              Buy Property Now!
+            </h2>
+            <p className="mb-4 text-sm md:text-xs">
               Ready to make your homeownership dreams a reality? Explore a
               diverse range of properties tailored to your preferences and
               budget.
             </p>
             <a
               href="./property-for-sale"
-              className="button1 read-more inline-flex items-center justify-center px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+              className="button1 read-more inline-flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-sm md:text-xs"
             >
               Browse Properties
               <span className="sr-only">about this is some title</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5 ml-2"
+                className="w-4 h-4 ml-2"
                 viewBox="0 0 20 20"
                 fill="currentColor"
               >
@@ -89,21 +91,23 @@ export default function HowItWorks() {
             />
           </figure>
           <div className="article-body text-center">
-            <h2 className="text-2xl font-bold mb-4">Discover Our Story!</h2>
-            <p className="mb-4">
+            <h2 className="text-xl md:text-lg font-bold mb-4">
+              Discover Our Story!
+            </h2>
+            <p className="mb-4 text-sm md:text-xs">
               We are your trusted partners in finding your perfect home. <br />
               Discover our story and let us help you write the next chapter of
               yours.
             </p>
             <a
               href="./aboutus"
-              className="button1 read-more inline-flex items-center justify-center px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+              className="button1 read-more inline-flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-sm md:text-xs"
             >
               Learn More
               <span className="sr-only">about this is some title</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5 ml-2"
+                className="w-4 h-4 ml-2"
                 viewBox="0 0 20 20"
                 fill="currentColor"
               >
