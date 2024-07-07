aafb105570f772d02d9ca8cf6b0252eeb99cf8e8
Daniel Akilo <kayodeakilo1@gmail.com>
2024-07-07 21:39:34 +0200

    changing layout of cards in howitworks

--- main-app/app/_components/HowItWorks.tsx
+++ main-app/app/_components/HowItWorks.tsx
@@ -3,9 +3,9 @@ import cta1 from "@/ctapic1/r1.jpg";
 
 export default function HowItWorks() {
   return (
-    <section className="pt-14 gap-5 max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3">
-      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 w-full md:w-full h-56 md:h-auto mx-auto mb-8">
-        <div className="article-wrapper p-4 h-full flex flex-col justify-between">
+    <section className="pt-14 gap-5 max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
+      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 w-11/12 md:w-full mx-auto">
+        <div className="article-wrapper p-4">
           <figure className="mb-4">
             <img
               src="./ctapic2.jpg"
@@ -14,23 +14,22 @@ export default function HowItWorks() {
             />
           </figure>
           <div className="article-body text-center">
-            <h2 className="text-sm md:text-lg font-bold mb-2 md:mb-4">
+            <h2 className="text-xl md:text-lg font-bold mb-2 md:mb-4">
               Sell Property
             </h2>
-            <p className="mb-4 text-xs md:text-sm">
+            <p className="mb-4 text-sm md:text-sm">
               Sell Your Property Now! Personalized property creation. Control
               all your properties from the tips of your fingertips.
               <br /> Do not miss out on your home being sold – Sell today!
             </p>
             <a
               href="./sign-in"
-              className="button1 read-more inline-flex items-center justify-center px-2 py-1 md:px-4 md:py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-xs md:text-sm"
+              className="button1 read-more inline-flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-sm md:text-xs"
             >
-              Sign UP
-              <span className="sr-only">about this is some title</span>
+              Sign UP<span className="sr-only">about this is some title</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-3 h-3 md:w-4 md:h-4 ml-2"
+                className="w-4 h-4 ml-2"
                 viewBox="0 0 20 20"
                 fill="currentColor"
               >
@@ -44,7 +43,7 @@ export default function HowItWorks() {
           </div>
         </div>
       </article>
-      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 w-full md:w-full h-56 md:h-auto mx-auto mb-8">
+      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 w-11/12 md:w-full mx-auto">
         <div className="article-wrapper p-4 h-full flex flex-col justify-between">
           <figure className="mb-4">
             <img
@@ -54,23 +53,23 @@ export default function HowItWorks() {
             />
           </figure>
           <div className="article-body text-center">
-            <h2 className="text-sm md:text-lg font-bold mb-2 md:mb-4">
+            <h2 className="text-xl md:text-lg font-bold mb-2 md:mb-4">
               Buy Property Now!
             </h2>
-            <p className="mb-4 text-xs md:text-sm">
+            <p className="mb-4 text-sm md:text-xs">
               Ready to make your homeownership dreams a reality? Explore a
               diverse range of properties tailored to your preferences and
               budget.
             </p>
             <a
               href="./property-for-sale"
-              className="button1 read-more inline-flex items-center justify-center px-2 py-1 md:px-4 md:py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-xs md:text-sm"
+              className="button1 read-more inline-flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-sm md:text-xs"
             >
               Browse Properties
               <span className="sr-only">about this is some title</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-3 h-3 md:w-4 md:h-4 ml-2"
+                className="w-4 h-4 ml-2"
                 viewBox="0 0 20 20"
                 fill="currentColor"
               >
@@ -84,8 +83,8 @@ export default function HowItWorks() {
           </div>
         </div>
       </article>
-      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 w-full md:w-full h-56 md:h-auto mx-auto mb-8">
-        <div className="article-wrapper p-4 h-full flex flex-col justify-between">
+      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 w-11/12 md:w-full mx-auto">
+        <div className="article-wrapper p-4">
           <figure className="mb-4">
             <img
               src="./ctapic5.jpg"
@@ -94,23 +93,23 @@ export default function HowItWorks() {
             />
           </figure>
           <div className="article-body text-center">
-            <h2 className="text-sm md:text-lg font-bold mb-2 md:mb-4">
+            <h2 className="text-xl md:text-lg font-bold mb-2 md:mb-4">
               Discover Our Story!
             </h2>
-            <p className="mb-4 text-xs md:text-sm">
+            <p className="mb-4 text-sm md:text-xs">
               We are your trusted partners in finding your perfect home. <br />
               Discover our story and let us help you write the next chapter of
               yours.
             </p>
             <a
               href="./aboutus"
-              className="button1 read-more inline-flex items-center justify-center px-2 py-1 md:px-4 md:py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-xs md:text-sm"
+              className="button1 read-more inline-flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-sm md:text-xs"
             >
               Learn More
               <span className="sr-only">about this is some title</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-3 h-3 md:w-4 md:h-4 ml-2"
+                className="w-4 h-4 ml-2"
                 viewBox="0 0 20 20"
                 fill="currentColor"
               >
