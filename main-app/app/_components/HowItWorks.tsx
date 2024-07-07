ee8a399f1f0be7cc991da1857b61c2589e65cc62
Daniel Akilo <kayodeakilo1@gmail.com>
2024-07-07 21:03:08 +0200

    changed inline for buttons

--- main-app/app/_components/HowItWorks.tsx
+++ main-app/app/_components/HowItWorks.tsx
@@ -3,8 +3,8 @@ import cta1 from "@/ctapic1/r1.jpg";
 
 export default function HowItWorks() {
   return (
-    <section className="pt-14 gap-5 max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
-      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 w-11/12 md:w-full mx-auto">
+    <section className="pt-14 gap-5 max-w-5xl mx-auto grid grid-cols-2 md:grid-cols-3">
+      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 mx-auto">
         <div className="article-wrapper p-4">
           <figure className="mb-4">
             <img
@@ -26,7 +26,8 @@ export default function HowItWorks() {
               href="./sign-in"
               className="button1 read-more inline-flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-sm md:text-xs"
             >
-              Sign UP<span className="sr-only">about this is some title</span>
+              Sign UP
+              <span className="sr-only">about this is some title</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-4 h-4 ml-2"
@@ -43,7 +44,7 @@ export default function HowItWorks() {
           </div>
         </div>
       </article>
-      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 w-11/12 md:w-full mx-auto">
+      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 mx-auto">
         <div className="article-wrapper p-4">
           <figure className="mb-4">
             <img
@@ -83,7 +84,7 @@ export default function HowItWorks() {
           </div>
         </div>
       </article>
-      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 w-11/12 md:w-full mx-auto">
+      <article className="rounded-lg shadow-lg bg-white transform transition-transform hover:scale-105 mx-auto">
         <div className="article-wrapper p-4">
           <figure className="mb-4">
             <img
